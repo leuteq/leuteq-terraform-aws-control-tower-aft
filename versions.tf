5f6b574d03d0079a637b475840de6bdcbb4884d2
saugattiwarii <saugat.tiwari@certegy.com>
2025-11-15 12:12:16 +0545

    update: required version changed

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@
 # Defines and manages the Terraform versions and required providers
 ################################################################################
 terraform {
-  required_version = ">= 1.7.0"
+  required_version = "~> 1.0"
 
   required_providers {
     aws = {
